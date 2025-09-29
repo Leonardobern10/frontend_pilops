8e6fcf3d9ae12771a76e0be231aa7531c1e47782
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-29 11:08:49 -0300

    new: componente para exibir info do aviao

---
+++ src/components/AircraftInfo.tsx
@@ -0,0 +1,29 @@
+import { Box, Typography } from '@mui/material';
+import type { ReactElement } from 'react';
+
+/**
+ * Componente responsável por exibir as informações do avião.
+ *
+ * @param name - Representa o nome do avião
+ * @param airline - Representa a linha aérea
+ */
+export default function AircraftInfo({
+    name,
+    airline
+}: {
+    name: string;
+    airline: string;
+}): ReactElement {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+                alignItems: 'center'
+            }}>
+            <Typography variant="body1">{name}</Typography>
+            <Typography variant="body2">{airline}</Typography>
+        </Box>
+    );
+}
