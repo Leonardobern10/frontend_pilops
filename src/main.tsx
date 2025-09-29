5be739d4bc520ecd35b27edcfb557173ac57dbda
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-29 11:10:21 -0300

    add: adicionando rotas

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from './theme/theme.ts';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import HistoryFlight from './view/HistoryFlight.tsx';
+import DetailsFlight from './view/DetailsFlight.tsx';
 
 createRoot(document.getElementById('root')!).render(
     // Componente de contexto do Material UI.
@@ -10,7 +13,14 @@ createRoot(document.getElementById('root')!).render(
         {/* É um reset global de CSS que o MUI aplica*/}
         <CssBaseline />
         <StrictMode>
-            <App />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/flight" element={<App />}>
+                        <Route index element={<HistoryFlight />} />
+                        <Route path=":pid" element={<DetailsFlight />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
         </StrictMode>
     </ThemeProvider>
 );
