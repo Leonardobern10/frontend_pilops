6dfc8b80ea0b34aa375e47f78b46d4901286d2f8
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-29 11:09:46 -0300

    add: comentarios

---
+++ src/components/LogoComponent.tsx
@@ -0,0 +1,20 @@
+import { Box } from '@mui/material';
+import logo from '../assets/Logo_pilops.svg';
+
+/**
+ * Componente responsável por exibir a logo da empresa.
+ * @returns Logo da empresa.
+ */
+export default function LogoComponent() {
+    return (
+        <Box
+            sx={{
+                width: 'fit-content',
+                height: 'fit-content',
+                borderColor: '#F00',
+                border: 0.5
+            }}>
+            <img src={logo} alt="Logo" />
+        </Box>
+    );
+}
