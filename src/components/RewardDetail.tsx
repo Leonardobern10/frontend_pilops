7482317a38d25d388480f75b635da2676201b833
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-09-30 10:57:52 -0300

    feat: tratamento de dados de acordo com o tipo a ser exibido e add: Adicionando comentários

--- src/components/RewardDetail.tsx
+++ src/components/RewardDetail.tsx
@@ -2,14 +2,53 @@ import { Box, Typography } from '@mui/material';
 import type { ReactElement } from 'react';
 import type { RewardDetailProps } from '../types/RewardDetailProps';
 
+/**
+ * Componente genérico responsável por exibir detalhes
+ * das recompensas obtidas em um vôo específico.
+ *
+ * @param iconSrc - Uma `string` com caminho relativo para um ícone a ser renderizado
+ * @param iconAlt - Uma `string` que contém o texto alternativo para um icone a ser renderizado
+ * @param title - Representa uma `string` que será exibida como titulo da recompensa obtida
+ * @param value - Representa um `number` que será exibido como valor da recompensa obtida
+ * @param uppercase - Representa um `boolean` que indica se o titulo deve ser em letras maiusculas.
+ * @default false
+ * @param type - Representa o tipo de informação que o componente carrega `'number' | 'balance' | 'percentage'`.
+ * `number` para numeros, `balance` para saldo, `percentage` para porcentagem.
+ * @returns
+ */
 export default function RewardDetail({
     iconSrc,
     iconAlt,
     title,
     value,
     uppercase,
-    percentage
+    type = 'number'
 }: RewardDetailProps): ReactElement {
+    const checkBalance = () => {
+        switch (type) {
+            case 'number':
+                return <Typography variant="h1">{value}</Typography>;
+            case 'balance':
+                let result = Math.abs(value)
+                    .toLocaleString('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    })
+                    .slice(2);
+                return (
+                    <Typography
+                        color={`${value > 0 ? 'success' : 'error'}`}
+                        variant="h1">
+                        {result}
+                    </Typography>
+                );
+            case 'percentage':
+                return <Typography variant="h1">{`${value}%`}</Typography>;
+            default:
+                return <Typography variant="h1">{value}</Typography>;
+        }
+    };
+
     return (
         <Box
             sx={{
@@ -29,7 +68,7 @@ export default function RewardDetail({
                 <Typography variant="subtitle2">
                     {uppercase ? title.toUpperCase() : title}
                 </Typography>
-                <Typography variant="h1">{`${value}${percentage ? '%' : ''}`}</Typography>
+                {checkBalance()}
             </Box>
         </Box>
     );
