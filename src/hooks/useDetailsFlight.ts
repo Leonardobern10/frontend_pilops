1bf2f47b6f268d6918c3c4ac38bfda837f3dee05
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-10-01 10:15:52 -0300

    feat: hook que obtém os detalhes de um voo

---
+++ src/hooks/useDetailsFlight.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import type FlightInterface from '../types/FlightInterface';
+import { getFlightDetails } from '../services/searchFlight';
+
+/**
+ * Hook personalizado que utiliza a função `getFlightDetails(id)`
+ * para realizar a requisição à api e obter os detalhes de um
+ * vôo específico.
+ *
+ * @param id - `string` identificador único de um registro de voo especifico.
+ * @returns `{current, loading}` - `current` são todos os dados de um vôo específico
+ * e `loading` informa o estado da requisição, se está em andamando `true` ou se
+ * encerrou `false`.
+ */
+export const useDetailsFlight = (id: string) => {
+    const [currentFlight, setCurrentFlight] = useState<FlightInterface>();
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const getCurrentFlight = async (id: string) => {
+            setLoading(true);
+            try {
+                let data = await getFlightDetails(id);
+                if (data) setCurrentFlight(data);
+                else throw new Error('Error on get a flight details');
+            } catch (error) {
+                console.error('Error on get a flight details: ', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        getCurrentFlight(id);
+    }, [id]);
+    return { current: currentFlight, loading };
+};
