6243e872ac14eec8497a822a01839afec3b66776
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-10-01 10:10:15 -0300

    update: separando estilização e add: adicionando comentários

--- src/components/DetailFlight.tsx
+++ src/components/DetailFlight.tsx
@@ -1,43 +1,33 @@
-import { Box, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import { type ReactElement } from 'react';
 import type { DetailFlightProps } from '../types/DetailFlightProps';
+import InfoFlightContainer from './ui/InfoFlightContainer';
+import { setSignal } from '../services/balanceService';
 
 /**
  * Componente responsável por exibir informações complementares do voo
  * @param title - Representa o título da informação
  * @param content - Representa o conteudo da informação
+ * @param balance - [OPCIONAL] `boolean` que indica se o componente deve tratar o valor
+ * recebido como um `balance` (saldo) ou não. @default false
  */
-export default function DetailFlight({
+export default function InfoFlight({
     title,
     content,
     balance
 }: DetailFlightProps): ReactElement {
-    const setSignal = () => {
-        let value = Number(content);
-        if (value < 0) return `- P$ ${Math.abs(value).toFixed(2)}`;
-        else return `P$ ${Math.abs(value).toFixed(2)}`;
-    };
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                whiteSpace: 'nowrap',
-                paddingLeft: 4,
-                paddingRight: 4
-            }}>
+        <InfoFlightContainer>
             <Typography variant="caption">{title}</Typography>
             {balance ? (
                 <Typography
                     variant="body1"
                     color={Number(content) > 0 ? 'success' : 'error'}>
-                    {setSignal()}
+                    {setSignal(content)}
                 </Typography>
             ) : (
                 <Typography variant="body1">{content}</Typography>
             )}
-        </Box>
+        </InfoFlightContainer>
     );
 }
