1531336dd4b9c339b964a307c20be9acd4d1c0bd
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-10-01 10:14:01 -0300

    new:  Componente container que abriga as informações de um registro no histórico de vôos.

---
+++ src/components/ui/CardActionAreaContainer.ts
@@ -0,0 +1,24 @@
+import { CardActionArea, styled } from '@mui/material';
+
+/**
+ * Componente container que abriga as informações de um registro
+ * no histórico de vôos.
+ *
+ * @extends - CardActionArea - Componente interativo do Material UI para
+ * cards com feedback visual e interatividade.
+ */
+const CardActionFlightContainer = styled(CardActionArea)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+    paddingLeft: theme.spacing(4),
+    paddingRight: theme.spacing(4),
+    paddingBottom: theme.spacing(2),
+    paddingTop: theme.spacing(2),
+    columnGap: theme.spacing(15)
+}));
+
+export default CardActionFlightContainer;
