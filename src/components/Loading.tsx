2b4091abd34c7370b808be77a2b07d0d93585a4a
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-10-01 10:12:41 -0300

    add: comentarios

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,11 +1,17 @@
 import { Box, CircularProgress, Typography } from '@mui/material';
 import type { ReactElement } from 'react';
 
+/**
+ * Componente responsável por representar o carregamento da página,
+ * gerando um feedback visual ao usuário.
+ *
+ * @returns `ReactElement`.
+ */
 export default function Loading(): ReactElement {
     return (
         <Box>
             <Typography variant="body1">
-                Loaging...
+                Loading...
                 <span>
                     <CircularProgress />
                 </span>
