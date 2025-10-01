3d6e4fa45913058f3a41128af3609f146181da39
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-10-01 10:17:20 -0300

    add: comentários

--- src/services/searchFlight.ts
+++ src/services/searchFlight.ts
@@ -3,6 +3,15 @@ import type FlightInterface from '../types/FlightInterface';
 
 const baseUrl: string = 'http://localhost:3000/api/v1/flights';
 
+/**
+ * Essa função é responsável por realizar uma requisição
+ * HTTP do tipo GET na api e entregar os dados do historico
+ * e também o número de paginas.
+ *
+ * @param currentPage - Página atual solicitada.
+ * @param limit - Quantidade de registros por página.
+ * @returns `Array` - Dados do registro em JSON `FlightInterface[]` e total de páginas `number`.
+ */
 export const getHistoryFlight = async (
     currentPage: number = 0,
     limit: number = 10
@@ -14,7 +23,17 @@ export const getHistoryFlight = async (
     return [flightHistory.data.flights, flightHistory.data.totalPages];
 };
 
-export const getFlightDetails = async (id: string | number) => {
+/**
+ * Essa função é responsável por realizar uma requisição
+ * HTTP do tipo GET na api e entregar os dados de um registro
+ * de vôo específico.
+ *
+ * @param id - Representa o identificador único `number` do registro a ser consultado
+ * @returns
+ */
+export const getFlightDetails = async (
+    id: string | number
+): Promise<FlightInterface> => {
     const flightDetails = await axios.get(`${baseUrl}/${id}`);
     return flightDetails.data.flight[0];
 };
