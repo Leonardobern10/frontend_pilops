dea934b1eea7f11c5e3dbeb60c2050e611075862
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-10-01 10:14:21 -0300

    new: Componente container que abriga toda a seção do historico de vôos

---
+++ src/components/ui/HistoryFlightContainer.ts
@@ -0,0 +1,16 @@
+import { Box, styled } from '@mui/material';
+
+/**
+ * Componente container que abriga toda a seção do historico de vôos
+ *
+ * @extends `Box`.
+ */
+const HistoryFlightContainer = styled(Box)(({ theme }) => ({
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    rowGap: theme.spacing(4)
+}));
+
+export default HistoryFlightContainer;
