dc990e451a7b0307102c4b3e67a4b8c76038eb31
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-10-01 10:17:33 -0300

    add: comentários

--- src/types/DetailFlightProps.ts
+++ src/types/DetailFlightProps.ts
@@ -1,3 +1,7 @@
+/**
+ * Representa a tipagem para a props que devem
+ * estar presenta no componente `DetailFlight`.
+ */
 export type DetailFlightProps = {
     title: string;
     content: string | number;
--- src/types/FlightDataInterface.ts
+++ src/types/FlightDataInterface.ts
@@ -1,5 +1,9 @@
 import type RouteInterface from './RouteInterface';
 
+/**
+ * Representa a tipagem para a propridade flightData,
+ * presente nos dados `flight`.
+ */
 export default interface FlightDataInterface {
     date: string;
     balance: number;
