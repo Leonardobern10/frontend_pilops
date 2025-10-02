c5f1f3c1679b8ab8aa8d012b67c28b85de86c57e
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-10-02 18:21:22 -0300

    add: adicionando novas rotas

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import theme from './theme/theme.ts';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 import HistoryFlight from './pages/HistoryFlight.tsx';
 import DetailsFlight from './pages/DetailsFlight.tsx';
+import ScoreFlight from './pages/ScoreFlight.tsx';
 
 createRoot(document.getElementById('root')!).render(
     // Componente de contexto do Material UI.
@@ -17,6 +18,7 @@ createRoot(document.getElementById('root')!).render(
 
                 <Route path="/flights" element={<App />}>
                     <Route index element={<HistoryFlight />} />
+                    <Route path="score" element={<ScoreFlight />} />
                     <Route path=":id" element={<DetailsFlight />} />
                 </Route>
             </Routes>
