272c9cf917704fca0c4089a4957d62acf70792e2
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-10-02 18:45:48 -0300

    add: adicionando comentários explicativos

--- src/hooks/useScoreFlights.ts
+++ src/hooks/useScoreFlights.ts
@@ -2,6 +2,20 @@ import { useEffect, useState } from 'react';
 import { getScore } from '../services/searchFlight';
 import type ScoreResponse from '../types/ScoreResponse';
 
+/**
+ * Hook personalizado que utiliza a função `getScore()`
+ * para realizar a requisição à api e obter os detalhes de
+ * pontuações referentes ao historio de voos.
+ *
+ * @returns `{currentScore}` - `currentScore` contém todos os
+ * dados correspondentes a dados estatísticos sobre pontuação
+ * `ScoreResponse`.
+ * - balance: `string` - Total de saldo atingido
+ * - xp: `string` - Total de xp adquiridos
+ * - missionBonus: `string` - Total de bonus adquiridos
+ * - maxBalance: `FlightInterface` - Registro de vôo de maior saldo
+ * - minBalance: `FlightInterface` - Registro de vôo de menor saldo
+ */
 export const useScoreFlights = (): ScoreResponse | null => {
     const [currentScore, setCurrentScore] = useState<ScoreResponse>();
 
