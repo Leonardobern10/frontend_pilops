c310a5719eecd977dfb7cb4253a5df436de8d238
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-10-03 20:00:05 -0300

    new: topo do history

---
+++ src/components/ui/HeaderHistoryContainer.ts
@@ -0,0 +1,10 @@
+import { Stack, styled } from '@mui/material';
+
+export const HeaderHistoryContainer = styled(Stack)(({ theme }) => ({
+    flexDirection: 'column',
+    rowGap: theme.spacing(4),
+    [theme.breakpoints.up('md')]: {
+        justifyContent: 'space-between',
+        flexDirection: 'row'
+    }
+}));
